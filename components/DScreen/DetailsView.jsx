eb86b7005df216ceaa7c37afcf125ea235dde742
techgeoplanet <shubham@geoplanetsolution.com>
2024-05-04 18:45:43 +0530

    water level statics set

--- components/DScreen/DetailsView.jsx
+++ components/DScreen/DetailsView.jsx
@@ -1,11 +1,22 @@
-import React, {useState}from 'react';
-import { View, StyleSheet, Text, TouchableOpacity ,Alert} from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, StyleSheet, Text, TouchableOpacity, Alert, Image, Linking } from 'react-native';
 import { useRoute } from '@react-navigation/native'; // Import useRoute hook from React Navigation
 import Vcolor from '../../global';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 import WaterDropIcon from '../svg/WaterDrop';
 import DateSelectorModal from '../Module/DatePicker';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
 
 
 const DetailsViewScrFun = () => {
@@ -38,107 +49,270 @@ const DetailsViewScrFun = () => {
     }
   };
 
+  const GraphData = {
+    labels: [
+      " 1", " 2", " 3", " 4", " 5", " 6",
+    " 7", " 8", " 9", " 10", " 11", " 12",
+    " 13", " 14", " 15", " 16", " 17", " 18",
+    " 19", " 20", " 21", " 22", " 23", " 24",
+    " 25", " 26", " 27", " 28",
+    ],
+    datasets: [
+      {
+        data: [
+          20, 45, 28, 80, 99, 43, 65, 30, 55, 75, 88, 22, 60, 45, 35, 70, 42,
+          95, 33, 50, 25, 90, 40, 15, 75, 65, 80
+        ],
+        color: (opacity = 1) =>'white', // optional
+        strokeWidth: 3 // optional
+      }
+    ],
+    legend: ["Water Level Static"] // optional
+  };
+
+  const handleMapPress = () => {
+    const latitude = 26.892606253280935; // Example latitude
+    const longitude =  75.81535673412989; // Example longitude
+    const mapUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+    Linking.openURL(mapUrl);
+  };
+
+
   return (
-    <View style={{ flex: 1, backgroundColor:Vcolor.liteprimary,justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{backgroundColor:'white',width:'100%',height:'100%',borderTopLeftRadius:20,borderTopRightRadius:20,paddingTop:10,paddingHorizontal:10}}>
+    <View style={{ flex: 1, backgroundColor: Vcolor.liteprimary, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, backgroundColor: 'white', borderTopLeftRadius: 20, borderTopRightRadius: 20, paddingTop: 10, paddingHorizontal: 10 }}>
         {/* data section */}
-          <DateSelectorModal
-            isVisible={isModalVisible}
-            onClose={closeModal}
-            onDateSelect={handleDateSelect}
-          />
+        <DateSelectorModal
+          isVisible={isModalVisible}
+          onClose={closeModal}
+          onDateSelect={handleDateSelect}
+        />
 
         {/* water hole specigic Main View  */}
-        <View style={{width:'100%',height:'100%'}}>
+        <ScrollView style={{ flex: 1, }}>
 
           {/* date selection btn  */}
 
-          <View style={{flexDirection:'row', width:'100%',justifyContent:'center',alignItems:'center'}}>
-            <Text style={{flex:2,color:Vcolor.darkprimary,fontWeight:500}}>Select Date :</Text>
+          <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Text style={{ color: Vcolor.darkprimary, fontWeight: 500 }}>Select Date :</Text>
             <TouchableOpacity
               onPress={() => { setSelectedDate('FromDate'); openModal(); }}
             >
-              <Text style={{color:'white',borderWidth:2,borderColor:Vcolor.primary,borderRadius:50,padding:5,width:150,fontWeight:500,textAlign:'center',backgroundColor:Vcolor.liteprimary,elevation:20}}>
-              {FromDate}
+              <Text style={{ color: 'white', borderWidth: 2, borderColor: Vcolor.primary, borderRadius: 50, padding: 5, width: 110, fontWeight: 500, textAlign: 'center', backgroundColor: Vcolor.liteprimary, elevation: 2 }}>
+                {FromDate}
               </Text>
             </TouchableOpacity>
 
-            <Text style={{flex:2,color:Vcolor.darkprimary,fontWeight:500,textAlign:'center'}}>To :</Text>
+            <Text style={{ color: Vcolor.darkprimary, fontWeight: 500, textAlign: 'center' }}>To :</Text>
 
             <TouchableOpacity
               onPress={() => { setSelectedDate('ToDate'); openModal(); }}
             >
-              <Text style={{color:'white',borderWidth:2,borderColor:Vcolor.primary,borderRadius:50,padding:5,width:150,fontWeight:500,textAlign:'center',backgroundColor:Vcolor.liteprimary,elevation:20}}>
-              {ToDate}
+              <Text style={{ color: 'white', borderWidth: 2, borderColor: Vcolor.primary, borderRadius: 50, padding: 5, width: 110, fontWeight: 500, textAlign: 'center', backgroundColor: Vcolor.liteprimary, elevation: 2 }}>
+                {ToDate}
               </Text>
             </TouchableOpacity>
           </View>
-          
+
+
           {/* for water hole information  */}
-          <View style={{ marginTop: 10, height: 75, justifyContent: 'center', alignItems: 'center',elevation:10 }}>
-            <View style={{ width: '97%', height: '100%', borderWidth: 1, borderColor: Vcolor.liteprimary, flexDirection: 'row', borderRadius: 10, padding: 5, paddingHorizontal: 10 }}>
+          <View style={{ height: 300, marginTop: 20, justifyContent: 'center', alignItems: 'center', elevation: 10 }}>
+            <View style={{ width: '97%', height: '100%', borderWidth: 1, borderColor: Vcolor.liteprimary, borderRadius: 10, padding: 5, paddingHorizontal: 10 }}>
+              <View style={{ width: '100%', flexDirection: 'row', }}>
+                {/* water hole id or name and addresssection  */}
+                <View style={{ flex: 6, justifyContent: 'space-around', alignItems: 'flex-start' }}>
+                  {/* water hole  name or parrent name section  */}
+                  <View style={{ flexDirection: 'row' }}>
+                    {/* water hole id  */}
+                    <View style={{ flexDirection: 'row', marginRight: 10, justifyContent: 'center', alignItems: 'center' }}>
+                      <Text style={{ fontSize: 14, color: Vcolor.darkprimary, marginEnd: 5 }}>WH0002</Text>
+                      <FontAwesomeIcon name="feed" size={15} color="green" solid />
+                    </View>
 
-              {/* water hole id or name and addresssection  */}
-              <View style={{ flex: 6, justifyContent: 'space-around', alignItems: 'flex-start' }}>
-                {/* water hole  name or parrent name section  */}
-                <View style={{ flexDirection: 'row' }}>
-                  {/* water hole id  */}
-                  <View style={{ flexDirection: 'row', marginRight: 10, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ fontSize: 14, color: Vcolor.darkprimary, marginEnd: 5 }}>WH0002</Text>
-                    <FontAwesomeIcon name="feed" size={15} color="green" solid />
+                    {/* water hole parrent id  */}
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                      <Text style={{ fontSize: 14, color: Vcolor.darkprimary, marginEnd: 5 }}>Host: WHM001</Text>
+                      <FontAwesomeIcon name="podcast" size={15} color="green" solid />
+                    </View>
+
                   </View>
 
-                  {/* water hole parrent id  */}
+                  {/* water hole address section  */}
                   <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ fontSize: 14, color: Vcolor.darkprimary, marginEnd: 5 }}>Host: WHM001</Text>
-                    <FontAwesomeIcon name="podcast" size={15} color="green" solid />
+                    <FontAwesomeIcon name="map-marker" size={25} color="red" solid />
+                    <Text style={{ marginLeft: 5, color: Vcolor.darkprimary }}>Nayala bag Zone 2</Text>
+                  </View>
+                </View>
+                {/* water hole level section  */}
+                <View style={{ flex: 2.5, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+                  {/* for water drop level show icon svg  */}
+                  <View>
+                    <WaterDropIcon width={50} height={50} levelfrom={2} />
                   </View>
 
-                </View>
+                  {/* for presentage level section  */}
+                  <View style={{ alignItems: 'flex-end', justifyContent: 'center' }}>
 
-                {/* water hole address section  */}
+                    {/* for level presentage  */}
+                    <View style={{ marginRight: 5, marginBottom: 10 }}>
+                      <Text style={{ color: 'red' }}>10 %</Text>
+                    </View>
+                    {/* for View more btn  */}
+                  </View>
+                </View>
+              </View>
+
+
+              <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginTop: 20 }}>
+
+                {/* lat long  */}
                 <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                  <FontAwesomeIcon name="map-marker" size={15} color="red" solid />
-                  <Text style={{ marginLeft: 5, color: Vcolor.darkprimary }}>Nayala bag Zone 2</Text>
+                  <FontAwesomeIcon name="map-marker" size={25} color="red" solid />
+                  <Text style={{ marginLeft: 5, color: Vcolor.darkprimary }}>Lat : { }, Long : { }</Text>
+                </View>
+
+                {/* go through map   */}
+                <View style={{ flex: 1, height: 30, borderWidth: 1, borderColor: Vcolor.primary, borderRadius: 20, maxWidth: 200 }}>
+                  <TouchableOpacity style={{ width: '100%', height: '100%', alignItems: 'flex-end', flexDirection: 'row' }}
+                    onPress={handleMapPress} // Pass item ID to Details screen
+                  >
+                    {/* view text  */}
+                    <Text style={{ color: Vcolor.primary, fontSize: 12, fontWeight: 400, textAlign: 'center', textAlignVertical: 'center', height: '100%', flex: 1 }}>Show on map</Text>
+                    {/* reightarrow icon  */}
+                    <View style={{ backgroundColor: Vcolor.primary, borderTopStartRadius: 40, borderTopEndRadius: 40, borderBottomEndRadius: 40, height: '100%', width: 25, justifyContent: 'center', alignItems: 'center' }}>
+                      <AntDesignIcon name="right" size={20} color="white" solid />
+                    </View>
+                  </TouchableOpacity>
                 </View>
               </View>
 
-              {/* water hole level section  */}
-              <View style={{ flex: 2.5, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
-                {/* for water drop level show icon svg  */}
-                <View>
-                  <WaterDropIcon width={50} height={50} levelfrom={2} />
-                </View>
-
-                {/* for presentage level section  */}
-                <View style={{ alignItems: 'flex-end', justifyContent: 'center' }}>
-
-                  {/* for level presentage  */}
-                  <View style={{ marginRight: 5, marginBottom: 10 }}>
-                    <Text style={{ color: 'red' }}>10 %</Text>
-                  </View>
-                  {/* for View more btn  */}
-                  {/* view btn box  */}
-                  <View style={{ width: 60, height: 20, borderWidth: 1, borderColor: Vcolor.primary, borderRadius: 20 }}>
-                    <TouchableOpacity style={{ width: '100%', height: '100%', alignItems: 'flex-end', flexDirection: 'row' }}
-                     onPress={() => navigation.navigate('Details', { itemId: item.id })} // Pass item ID to Details screen
-                    >
-                      {/* view text  */}
-                      <Text style={{ color: Vcolor.primary, fontSize: 12, fontWeight: 400, textAlign: 'center', textAlignVertical: 'center', height: '100%', flex: 1 }}>View</Text>
-                      {/* reightarrow icon  */}
-                      <View style={{ backgroundColor: Vcolor.primary, borderTopStartRadius: 20, borderTopEndRadius: 20, borderBottomEndRadius: 20, height: '100%', width: 15, justifyContent: 'center', alignItems: 'center' }}>
-                        <AntDesignIcon name="right" size={15} color="white" solid />
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-                </View>
+              <View style={{ flex: 1, borderBottomStartRadius: 10, borderBottomEndRadius: 10, marginTop: 20, overflow: 'hidden', elevation: 10 }}>
+                {/* water hole image  */}
+                <Image
+                  source={require('../../assets/image/OWI2.jpg')}
+                  style={{ height: '100%', width: '100%' }}
+                  resizeMode="stretch"
+                />
               </View>
+
             </View>
           </View>
 
-        </View>
-        <Text>Details Screen</Text>
-      </View> 
+          {/* water hole condition comments  */}
+          <View style={{ justifyContent: 'center' }}>
+            {/* comments level  */}
+            <View style={{ padding: 5, backgroundColor: Vcolor.darkprimary, borderRadius: 20, marginVertical: 10 }}>
+              <Text style={{ color: 'white', fontWeight: 500, fontSize: 14, textAlign: 'center', textAlignVertical: 'center' }}>Comments :</Text>
+            </View>
+
+            {/* need repair  */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', padding: 5 }}>
+
+              <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+                <View>
+                  <Text style={{ color: Vcolor.darkprimary, fontWeight: 500, fontSize: 14 }}>Need Repair :</Text>
+                  <Text>Submitted by :</Text>
+                  <Text>shubham</Text>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'space-around' }}>
+                  <TouchableOpacity style={{ backgroundColor: 'red', width: 150, justifyContent: 'center', borderRadius: 20, padding: 5 }}><Text style={{ textAlign: 'center', fontWeight: 500, color: 'white' }}>Decline</Text></TouchableOpacity>
+                  <TouchableOpacity style={{ backgroundColor: Vcolor.darkprimary, width: 150, justifyContent: 'center', borderRadius: 20, padding: 5 }}><Text style={{ textAlign: 'center', fontWeight: 500, color: 'white' }}>Approve</Text></TouchableOpacity>
+                </View>
+              </View>
+
+              <View>
+                <Image
+                  source={require('../../assets/image/OWI.png')}
+                  style={{ height: 150, width: 200, borderRadius: 10 }}
+                  resizeMode="stretch"
+                />
+              </View>
+
+            </View>
+            {/* Last Cleaning Date  */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', padding: 5 }}>
+
+              <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+                <View>
+                  <Text style={{ color: Vcolor.darkprimary, fontWeight: 500, fontSize: 14 }}>Last Cleaning Date :</Text>
+                  <Text>Submitted by :</Text>
+                  <Text>shubham</Text>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'space-around' }}>
+                  <TouchableOpacity style={{ backgroundColor: 'red', width: 150, justifyContent: 'center', borderRadius: 20, padding: 5 }}><Text style={{ textAlign: 'center', fontWeight: 500, color: 'white' }}>Decline</Text></TouchableOpacity>
+                  <TouchableOpacity style={{ backgroundColor: Vcolor.darkprimary, width: 150, justifyContent: 'center', borderRadius: 20, padding: 5 }}><Text style={{ textAlign: 'center', fontWeight: 500, color: 'white' }}>Need For Cleaning</Text></TouchableOpacity>
+                </View>
+              </View>
+
+              <View>
+                <Image
+                  source={require('../../assets/image/OWI.png')}
+                  style={{ height: 150, width: 200, borderRadius: 10 }}
+                  resizeMode="stretch"
+                />
+              </View>
+
+            </View>
+            {/* Last filling date  */}
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', padding: 5 }}>
+
+              <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+                <View>
+                  <Text style={{ color: Vcolor.darkprimary, fontWeight: 500, fontSize: 14 }}>Last filling Date :</Text>
+                  <Text>Submitted by :</Text>
+                  <Text>shubham</Text>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'space-around' }}>
+                  <TouchableOpacity style={{ backgroundColor: 'red', width: 150, justifyContent: 'center', borderRadius: 20, padding: 5 }}><Text style={{ textAlign: 'center', fontWeight: 500, color: 'white' }}>Decline</Text></TouchableOpacity>
+                  <TouchableOpacity style={{ backgroundColor: Vcolor.darkprimary, width: 150, justifyContent: 'center', borderRadius: 20, padding: 5 }}><Text style={{ textAlign: 'center', fontWeight: 500, color: 'white' }}>Need For Cleaning</Text></TouchableOpacity>
+                </View>
+              </View>
+
+              <View>
+                <Image
+                  source={require('../../assets/image/OWI.png')}
+                  style={{ height: 150, width: 200, borderRadius: 10 }}
+                  resizeMode="stretch"
+                />
+              </View>
+
+            </View>
+
+          </View>
+
+          {/* Water hole graph report  */}
+
+          <LineChart
+            data={GraphData}
+            width={screenWidth - 20}
+            height={256}
+            verticalLabelRotation={-90}
+            chartConfig={{
+              backgroundColor: "red",
+              backgroundGradientFrom: Vcolor.darkprimary,
+              backgroundGradientTo: Vcolor.darkprimary,
+              decimalPlaces: 0, // optional, defaults to 2dp
+              color: (opacity = 0.6) => `rgba(255,255,255,${opacity})`,
+              labelColor: (opacity = 1) => `white`,
+              style: {
+                borderRadius: 16
+              },
+              propsForDots: {
+                r: "4",
+                strokeWidth: "1",
+                stroke: 'red'
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
+
+        </ScrollView>
+      </View>
     </View>
   );
 };
